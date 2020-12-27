4ddb02b64d610810de91d8141e1cde3cc2d33333
SieteNred <sietenred@gmail.com>
2020-12-27 11:12:19 +0100

    agregando constantes

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-const newYears = '1st Jan 2021'
+const daysEl =document.getElementById("days");
+const hoursEl =document.getElementById("hours");
+const minsEl =document.getElementById("mins");
+const secondsEl =document.getElementById("seconds");
+
+
+
+
+const newYears = '1 Jan 2021'
 function countdown () {
     const newYearsDate = new Date (newYears);
     const currentDate = new Date ();
 
-    console.log(newYearsDate - currentDate);
+    const totalSeconds = new Date(newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(seconds / 3600 / 24)
+
+    const hours = Math.floor(seconds / 3600) % 24;
+
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    console.log(days, hours, minutes, seconds);
 
 }
 
