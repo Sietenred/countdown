b7b0fbdac321881a4b3aa7a5ed62a29da9cacc51
SieteNred <sietenred@gmail.com>
2020-12-28 10:47:50 +0100

    finish proyect

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-const daysEl =document.getElementById("days");
-const hoursEl =document.getElementById("hours");
-const minsEl =document.getElementById("mins");
-const secondsEl =document.getElementById("seconds");
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
 
 
 
-const newYears = '1 Jan 2021'
+const newYears = '10 MAY 2021'
 function countdown () {
     const newYearsDate = new Date (newYears);
     const currentDate = new Date ();
 
-    const totalSeconds = new Date(newYearsDate - currentDate) / 1000;
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-    const days = Math.floor(seconds / 3600 / 24)
+    const days = Math.floor(totalSeconds / 3600 / 24)
 
-    const hours = Math.floor(seconds / 3600) % 24;
+    const hours = Math.floor(totalSeconds / 3600) % 24;
 
-    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const mins = Math.floor(totalSeconds / 60) % 60;
     
     const seconds = Math.floor(totalSeconds) % 60;
 
-    console.log(days, hours, minutes, seconds);
+    console.log(days, hours, mins, seconds);
 
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
 }
-
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
 countdown ();
 
 setInterval(countdown, 1000);
